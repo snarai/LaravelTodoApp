354b49324029ccb4ff666eea31be8e5d5da41c86
snarai <funkastic.jazz@gmail.com>
2021-05-03 12:17:40 +0900

    BugFix
    Change values of variables

--- TodoApp/resources/views/todos/edit.blade.php
+++ TodoApp/resources/views/todos/edit.blade.php
@@ -14,16 +14,16 @@
         </div>
         <br />
         @endif
-        <form method="post" action="{{ route('todos.update', $todo->id) }}">
+        <form method="post" action="{{ route('todos.update', $todos->id) }}">
             @method('PATCH')
             @csrf
             <div class="form-group">
                 <label for="title">Todo:</label>
-                <input type="text" class="form-control" name="title" value={{ $todo->title }} />
+                <input type="text" class="form-control" name="title" value={{ $todos->title }} />
             </div>
             <div class="form-group">
                 <label for="memo">メモ:</label>
-                <input type="text" class="form-control" name="memo" value={{ $todo->memo }} />
+                <input type="text" class="form-control" name="memo" value={{ $todos->memo }} />
             </div>
 
             <button type="submit" class="btn btn-primary">変更</button>
